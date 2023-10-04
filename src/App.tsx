e208613a35eba88967367bea27942c7128cdb53a
Erik-TS <erik.t3600@hotmail.com>
2023-10-03 23:31:23 -0300

    Create password generator function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 function App() {
-  
-  return(
-    <h1>Hello World!</h1>
+
+  function generatePassword(size: number): string {
+    function getRandomInt(min: number, max: number): number {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function getCharList(size: number): Array<string> {
+      let charList: Array<string> = []
+
+      for (let i = 0; i < size; i++) charList.push(String.fromCharCode(getRandomInt(33, 126)))
+      return charList
+    }
+
+    let password = getCharList(size).toLocaleString()
+    return password
+  }
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
   )
 }
 
